1c16247c57075fcff065478b8920b13799a832d2
kristinazagaynova <71030427+kristinazagaynova@users.noreply.github.com>
2020-11-02 16:33:20 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,23 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    x=plaintext
+    k=keyword
+    d=0
+    for i in x:
+            if i.isalpha():
+                if ord(k[d])>=65 and ord(k[d]) <=90:
+                    if (((ord(i) + ord(k[d])%65) > 90) and (ord(i) <= 90)) or (((ord(i) + ord(k[d])%65) > 122 )and (ord(i) <= 122)):
+                        i=chr(ord(i)-26)
+                    i=chr(ord(i)+ord(k[d])%65)
+                elif ord(k[d])>=97 and ord(k[d]) <=122:
+                    if (((ord(i) + ord(k[d])%97) > 90) and (ord(i) <= 90))or (((ord(i) + ord(k[d])%97)>122) and (ord(i) <= 122)):
+                        i=chr(ord(i)-26)    
+                    i=chr(ord(i)+ord(k[d])%97)
+                d=d+1
+                if d==len(k):
+                    d=0
+            ciphertext=ciphertext+i
     return ciphertext
 
 
@@ -26,5 +42,21 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    x=ciphertext
+    k=keyword
+    d=0
+    for i in x:
+            if i.isalpha():
+                if ord(k[d])>=65 and ord(k[d]) <=90:
+                    if (((ord(i) - ord(k[d])%65) < 65) and (ord(i) >= 65)) or (((ord(i) - ord(k[d])%65) < 97)and (ord(i) >= 97)):
+                        i=chr(ord(i)+26)
+                    i=chr(ord(i)-ord(k[d])%65)
+                elif ord(k[d])>=97 and ord(k[d]) <=122:
+                    if (((ord(i) - ord(k[d])%97)  <65) and (ord(i) >= 65))or (((ord(i) - ord(k[d])%97)<97) and (ord(i) >= 97)):
+                        i=chr(ord(i)+26)    
+                    i=chr(ord(i)-ord(k[d])%97)
+                d=d+1
+                if d==len(k):
+                    d=0
+            plaintext=plaintext+i
     return plaintext
